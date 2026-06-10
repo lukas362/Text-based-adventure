15e73fd0c964ec4e1d95d1a20c8e243bc2a1dd56
lukas362 <lukassvensson761@gmail.com>
2026-06-10 10:54:31 +0200

    Made some changes to the diffrent roles and also added other stats

--- game.py
+++ game.py
@@ -11,14 +11,32 @@ def role():
 
     if selection == "wizard staff":
         inventory.append("wizard_staff")
+        stats["hp"] = 25
+        stats["max_hp"] = 50
+        stats["stamina"] = 40
+        stats["max_stamina"] = 50
+        stats["mana"] = 100
+        stats["max_mana"] = 100
+        stats["level"] = 1
+        stats["xp"] = 0
+
         stats["strength"] = 4
         stats["Agility"] = 2
         stats["Intelligence"] = 7
-        stats["Charisma"] = 3
+        stats["Charisma"] = 4
 
     elif selection == "sword and shield":
         inventory.append("sword")
         inventory.append("shield")
+        stats["hp"] = 50
+        stats["max_hp"] = 100
+        stats["stamina"] = 70
+        stats["max_stamina"] = 80
+        stats["mana"] = 10
+        stats["max_mana"] = 10
+        stats["level"] = 1
+        stats["xp"] = 0
+
         stats["strength"] = 7
         stats["Agility"] = 5
         stats["Intelligence"] = 3
@@ -27,10 +45,19 @@ def role():
     elif selection == "bow and arrow":
         inventory.append("bow")
         inventory.append("arrows")
+        stats["hp"] = 40
+        stats["max_hp"] = 75
+        stats["stamina"] = 50
+        stats["max_stamina"] = 60
+        stats["mana"] = 20
+        stats["max_mana"] = 20
+        stats["level"] = 1
+        stats["xp"] = 0
+
         stats["strength"] = 5
         stats["Agility"] = 7
         stats["Intelligence"] = 4
-        stats["Charisma"] = 3
+        stats["Charisma"] = 5
 
     else: 
         print("Invalid selection, please select a class.")
