805e572a970f6acccd5b41690b9c5f22286dd695
lukas362 <lukassvensson761@gmail.com>
2026-06-10 17:11:58 +0200

    Added the ground works for a menu system

--- game.py
+++ game.py
@@ -2,7 +2,8 @@ import pygame
 import colorama
 from role_select import role
 from story import story
+from menu import menu
 
 story()
 role()
-
+menu()
---
+++ menu.py
@@ -0,0 +1,5 @@
+def menu():
+    while True:
+        menu_open = input()
+        if menu_open.lower() == "menu":
+            input(" ")
