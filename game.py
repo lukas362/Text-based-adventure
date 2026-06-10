f5a766159ec36382ff78ecb3c5250601db10a37f
lukas362 <lukassvensson761@gmail.com>
2026-06-10 11:19:55 +0200

    Seperated the role selection into a separate file and added .gitignore to ignore some files

--- game.py
+++ game.py
@@ -1,67 +1,8 @@
 import pygame
+from role_select import role
 
 def start_of_story():
     print("You wake up dazed, confused and you can hear screaming voices around you. You look around and you can see goblins, orcs and humans fighting each other.")
-
-def role():
-    inventory = []
-    stats = {}
-
-    selection = input("You can see Wizard staff, sword and shield and a bow and arrow on the ground next to you. What do you pick up? (wizard staff, sword and shield, bow and arrow): \n").lower()
-
-    if selection == "wizard staff":
-        inventory.append("wizard_staff")
-        stats["hp"] = 25
-        stats["max_hp"] = 50
-        stats["stamina"] = 40
-        stats["max_stamina"] = 50
-        stats["mana"] = 100
-        stats["max_mana"] = 100
-        stats["level"] = 1
-        stats["xp"] = 0
-
-        stats["strength"] = 4
-        stats["Agility"] = 2
-        stats["Intelligence"] = 7
-        stats["Charisma"] = 4
-
-    elif selection == "sword and shield":
-        inventory.append("sword")
-        inventory.append("shield")
-        stats["hp"] = 50
-        stats["max_hp"] = 100
-        stats["stamina"] = 70
-        stats["max_stamina"] = 80
-        stats["mana"] = 10
-        stats["max_mana"] = 10
-        stats["level"] = 1
-        stats["xp"] = 0
-
-        stats["strength"] = 7
-        stats["Agility"] = 5
-        stats["Intelligence"] = 3
-        stats["Charisma"] = 4
-    
-    elif selection == "bow and arrow":
-        inventory.append("bow")
-        inventory.append("arrows")
-        stats["hp"] = 40
-        stats["max_hp"] = 75
-        stats["stamina"] = 50
-        stats["max_stamina"] = 60
-        stats["mana"] = 20
-        stats["max_mana"] = 20
-        stats["level"] = 1
-        stats["xp"] = 0
-
-        stats["strength"] = 5
-        stats["Agility"] = 7
-        stats["Intelligence"] = 4
-        stats["Charisma"] = 5
-
-    else: 
-        print("Invalid selection, please select a class.")
-        return role()
 
 start_of_story()
 role()
---
+++ role_select.py
@@ -0,0 +1,59 @@
+def role():
+    inventory = []
+    stats = {}
+
+    selection = input("You can see Wizard staff, sword and shield and a bow and arrow on the ground next to you. What do you pick up? (wizard staff, sword and shield, bow and arrow): \n").lower()
+
+    if selection == "wizard staff":
+        inventory.append("wizard_staff")
+        stats["hp"] = 25
+        stats["max_hp"] = 50
+        stats["stamina"] = 40
+        stats["max_stamina"] = 50
+        stats["mana"] = 100
+        stats["max_mana"] = 100
+        stats["level"] = 1
+        stats["xp"] = 0
+
+        stats["strength"] = 4
+        stats["agility"] = 2
+        stats["intelligence"] = 7
+        stats["charisma"] = 4
+
+    elif selection == "sword and shield":
+        inventory.append("sword")
+        inventory.append("shield")
+        stats["hp"] = 50
+        stats["max_hp"] = 100
+        stats["stamina"] = 70
+        stats["max_stamina"] = 80
+        stats["mana"] = 10
+        stats["max_mana"] = 10
+        stats["level"] = 1
+        stats["xp"] = 0
+
+        stats["strength"] = 7
+        stats["agility"] = 5
+        stats["intelligence"] = 3
+        stats["charisma"] = 4
+    
+    elif selection == "bow and arrow":
+        inventory.append("bow")
+        inventory.append("arrows")
+        stats["hp"] = 40
+        stats["max_hp"] = 75
+        stats["stamina"] = 50
+        stats["max_stamina"] = 60
+        stats["mana"] = 20
+        stats["max_mana"] = 20
+        stats["level"] = 1
+        stats["xp"] = 0
+
+        stats["strength"] = 5
+        stats["agility"] = 7
+        stats["intelligence"] = 4
+        stats["charisma"] = 5
+
+    else: 
+        print("Invalid selection, please select a class.")
+        return role()
