401442b3e8f396792c1644c2cd99f1b692a8a574
Lukas Svensson <lukassvensson761@gmail.com>
2026-06-13 13:13:11 +0200

    Added some new lines to .gitignore + added comments to game.py for clarification on which python version to use for pygame and colorama.

--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+#To be able to run this with pygame, colorama and other modules you will need to use python 3.12.10 or lower, because the newer versions don't support pygame and colorama
+# You can do this using pip (for version 3.12.10 of python):
+# py -3.12 -m pip install pygame
+# py -3.12 -m pip install colorama
+
 import pygame
 import colorama
 from role_select import role
