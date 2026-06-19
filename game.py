3bb4b4eb97c72958db9e005ab6fcf83264a31d51
Lukas Svensson <lukassvensson761@gmail.com>
2026-06-19 14:50:00 +0200

    Made new directory to store code to make everything more organized

--- game.py
+++ game.py
@@ -5,9 +5,9 @@
 
 import pygame
 import colorama
-from role_select import role
+from src.role_select import role
 from story import story
-from menu import menu
+from src.menu import menu
 
 story()
 role()
