595c6b66303b0b385e7ff1533587cb559a604e0c
Lukas Svensson <lukassvensson761@gmail.com>
2026-06-23 15:24:11 +0200

    Commented out the import of pygame + removed old comments

--- game.py
+++ game.py
@@ -2,7 +2,6 @@
 
 #To be able to run this with pygame, colorama and other modules you will need to use python 3.12.10 or lower, because the newer versions don't support pygame and colorama
 # You can do this using pip (for version 3.12.10 of python):
-# py -3.12 -m pip install pygame
 # py -3.12 -m pip install colorama
 
 import colorama
